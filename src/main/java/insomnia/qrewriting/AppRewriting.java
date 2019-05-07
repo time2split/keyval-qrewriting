3a7f778a7b7b781d78441520e3946a0b490f8389
zuri <webzuri@gmail.com>
2019-05-07 11:03:17 +0200

    AppRewriting, add mergeQueries(), avoid showing codes after a merge operation

--- src/main/java/insomnia/qrewriting/AppRewriting.java
+++ src/main/java/insomnia/qrewriting/AppRewriting.java
@@ -10,6 +10,8 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
@@ -17,10 +19,12 @@ import java.util.concurrent.ExecutionException;
 import org.apache.commons.io.IOUtils;
 
 import insomnia.numeric.Interval;
+import insomnia.qrewriting.code.Code;
 import insomnia.qrewriting.code.Encoding;
 import insomnia.qrewriting.context.AppContext;
 import insomnia.qrewriting.context.Context;
 import insomnia.qrewriting.database.Driver;
+import insomnia.qrewriting.database.DriverException;
 import insomnia.qrewriting.database.driver.DriverQueryBuilder;
 import insomnia.qrewriting.database.driver.DriverQueryManager;
 import insomnia.qrewriting.generator.CodeGenerator;
@@ -144,16 +148,27 @@ public class AppRewriting
 	{
 		makeQueries();
 
+		Collection<Query> queries = mergeQueries(this.queries);
 		ArrayList<QueryBucket> ret = new ArrayList<>();
 		long                   i   = encoding.generateCodeInterval().geta();
 
 		QueryManager queryManager = driver.getAQueryManager();
 
+		if (queriesAreMerged)
+		{
+			Code _0code = encoding.getCodeFrom(0);
+
+			for (Query q : queries)
+				ret.add(new QueryBucket(q, i, _0code, queryManager));
+		}
+		else
+		{
 			for (Query q : queries)
 			{
 				ret.add(new QueryBucket(q, i, encoding.getCodeFrom((int) (i)), queryManager));
 				i++;
 			}
+		}
 		return ret;
 	}
 
@@ -209,28 +224,35 @@ public class AppRewriting
 		}
 		else
 			throw new InvalidParameterException("Bad parameter sys.nbThreads=" + getOption("sys.nbThreads"));
-	}
 
-		String optVal = options.getProperty("queries.merge.sizeOfQuery");
-
-		if (optVal != null)
-			merge:
-			{
-		final int size = Integer.parseInt(optVal);
-
-		if (size == 1)
-					break merge;
-		if (size == 0)
-				{
-					queries.clear();
-					break merge;
-				}
-		DriverQueryManager manager = driver.getAQueryManager();
-		Query[] tmp = manager.mergeBySizeOfQueries(size, queries);
-				queries = new ArrayList<>(Arrays.asList(tmp));
-			}
 		end = Instant.now();
 		times.put("computation", Duration.between(start, end));
+	}
+
+	private Collection<Query> mergeQueries(Collection<Query> queries) throws DriverException
+	{
+		String optVal = options.getProperty("queries.merge.sizeOfQuery");
+
+		if (optVal == null)
+			return queries;
+
+		final int size = Integer.parseInt(optVal);
+		queriesAreMerged = true;
+
+		if (size == 1)
+			return queries;
+
+		if (size == 0)
+			return Collections.emptyList();
+
+		DriverQueryManager manager = driver.getAQueryManager();
+
+		if (!manager.canMerge())
+			throw new DriverException("The query merge operation is not implemented by " + manager.getClass().getCanonicalName());
+
+		// TODO: change this in sort of disjonctivee query
+		Query[] tmp = manager.mergeBySizeOfQueries(size, queries);
+		return new ArrayList<>(Arrays.asList(tmp));
 	}
 
 	/**
